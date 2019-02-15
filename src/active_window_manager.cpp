b39ef8035dae681bc0d183c475935166497bdd6c
tomKPZ <tomKPZ@gmail.com>
2019-02-14 22:11:08 -0800

    Update copyright date

--- src/active_window_manager.cpp
+++ src/active_window_manager.cpp
@@ -1,5 +1,5 @@
 // rofi-workspace: An X11 workspace plugin for rofi
-// Copyright (C) 2018 <tomKPZ@gmail.com>
+// Copyright (C) 2019 <tomKPZ@gmail.com>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
