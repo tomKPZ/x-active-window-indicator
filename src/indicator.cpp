5fbdd123a14ee7c72904d116bde9bdcdc0a51921
tomKPZ <tomKPZ@gmail.com>
2019-03-05 15:36:10 -0800

    Run event loop

--- src/indicator.cpp
+++ src/indicator.cpp
@@ -1,3 +1,20 @@
+// x-active-window-indicator: An X11 utility that signals the active window
+// Copyright (C) 2019 <tomKPZ@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
 #include <xcb/xcb.h>
 
 #include <iostream>
@@ -8,6 +25,7 @@
 #include "active_window_tracker.h"
 #include "border_window.h"
 #include "connection.h"
+#include "event_loop.h"
 #include "key_listener.h"
 #include "key_state_observer.h"
 
@@ -47,6 +65,9 @@ int main(int, char**) {
   Connection connection;
   Indicator indicator{&connection};
   ActiveWindowTracker active_window_tracker_{&connection, &indicator};
-  KeyListener key_listener_{&connection, &indicator};
+  KeyListener key_listener{&connection, &indicator};
+  EventLoop loop{&connection};
+  loop.RegisterDispatcher(&key_listener);
+  loop.Run();
   return 0;
 }
