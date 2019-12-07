a4b9eb52a8473b44fe997d7093c2aa2a7623c273
tomKPZ <tomKPZ@gmail.com>
2019-12-07 14:13:15 -0800

    Adjust cppcheck flags

--- PRESUBMIT_destructive.sh
+++ PRESUBMIT_destructive.sh
@@ -9,8 +9,8 @@ iwyu-tool -p . -j8 -- -Xiwyu --mapping_file=iwyu.imp
 
 /usr/share/clang/run-clang-tidy.py . -fix -quiet
 
-cppcheck src --inconclusive --enable=all --std=posix --max-configs=1 -Isrc \
-	 -I/usr/include --quiet 2>&1 | grep -v "(information)" 1>&2
+cppcheck src --inconclusive --enable=all --force --suppress=unusedFunction \
+  --suppress=missingIncludeSystem >/dev/null
 
 clang-format -i src/*
 cmake-format -i CMakeLists.txt
